e0af700689e837b7bd539c766ddacb025d929517
JP Duplessis <jp@vteam.com>
2022-11-15 12:39:59 -0800

    responsive header

--- lib/widgets/header.dart
+++ lib/widgets/header.dart
@@ -14,19 +14,42 @@ class Header extends StatefulWidget {
 class _HeaderState extends State<Header> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-      child: Expanded(
-          child: Row(
-        children: [
-          Text(widget.title, style: Theme.of(context).textTheme.headline6),
-          const SizedBox(width: 10),
-          Text('(${widget.count})', style: Theme.of(context).textTheme.caption),
-          const Spacer(),
-          Text(widget.description, style: Theme.of(context).textTheme.caption),
-          const Text(""),
-        ],
-      )),
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth > 600) {
+          return _buildWide();
+        } else {
+          return _buildNarrow();
+        }
+      },
     );
+  }
+
+  _buildWide() {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+        child: Expanded(
+            child: Row(children: [
+          renderCaptionAndCount(widget.title, widget.count),
+          const Spacer(),
+          Text(widget.description, style: Theme.of(context).textTheme.caption)
+        ])));
+  }
+
+  _buildNarrow() {
+    return Padding(
+        padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+        child: Expanded(
+            child: Column(
+          children: [renderCaptionAndCount(widget.title, widget.count)],
+        )));
+  }
+
+  renderCaptionAndCount(caption, count) {
+    return Row(children: [
+      Text(widget.title, style: Theme.of(context).textTheme.headline6),
+      const SizedBox(width: 10),
+      Text('(${widget.count})', style: Theme.of(context).textTheme.caption)
+    ]);
   }
 }
