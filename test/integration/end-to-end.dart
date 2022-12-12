9be37d1613f1daccd8d244a404ec945fd5622566
JP <jp@vteam.com>
2022-12-12 13:01:46 -0800

    basic integration test

---
+++ test/integration/end-to-end.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+import 'package:money/main.dart' as app;
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('Landing page', (tester) async {
+      SharedPreferences.setMockInitialValues({});
+      app.main();
+      await tester.pumpAndSettle();
+
+      expect(find.text('MyMoney'), findsOneWidget);
+    });
+  });
+}
