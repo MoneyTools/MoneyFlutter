286267bdcf018c61bee34ecdb16336af46d58fb1
jpdup <jp.duplessis@teya.com>
2023-12-27 09:58:03 -0500

    layers.sh

---
+++ layers.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if which node >/dev/null; then
+  node_version=$(node -v)
+  echo "Found Node.js version: $node_version"
+  npx git@github.com:jpdup/glad.git --externals true -o layers.svg
+  open ./layers.svg
+else
+  echo "Node.js is not installed."
+  echo "To install use brew"
+  echo "brew install node"
+fi
+
+
