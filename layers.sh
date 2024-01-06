a16ced979e62d648a614da5d9a27a4064fc93627
jpdup <jp.duplessis@teya.com>
2024-01-06 15:58:35 -0500

    update layers

--- layers.sh
+++ layers.sh
@@ -3,7 +3,7 @@
 if which node >/dev/null; then
   node_version=$(node -v)
   echo "Found Node.js version: $node_version"
-  npx git@github.com:jpdup/glad.git -l --align left --lines elbow --view layers --externals true -o layers.svg
+  npx git@github.com:jpdup/glad.git -l --view layers --align left -o layers.svg
   open ./layers.svg
 else
   echo "Node.js is not installed."
