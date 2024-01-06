101e24d475a71ea6be684fa1b934e92673c19b5b
jpdup <jp.duplessis@teya.com>
2024-01-06 18:38:02 -0500

    use SegmentControl for bottom Tabs

--- lib/widgets/details_panel.dart
+++ lib/widgets/details_panel.dart
@@ -22,63 +22,65 @@ class DetailsPanel extends StatelessWidget {
 
   @override
   Widget build(final BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        const Divider(thickness: 1, height: 1),
-        _buildTabs(context),
-        if (isExpanded) _buildContent(),
-      ],
-    );
+    return LayoutBuilder(builder: (
+      final BuildContext context,
+      final BoxConstraints constraints,
+    ) {
+      return Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          const Divider(thickness: 1, height: 1),
+          _buildHeaderWithTabsAndExpando(context, constraints),
+          if (isExpanded) _buildContent(),
+        ],
+      );
+    });
   }
 
-  Widget getTabButton(final BuildContext context, final num id, final String text) {
-    final bool isSelected = selectedTabId == id;
+  Widget _buildHeaderWithTabsAndExpando(
+    final BuildContext context,
+    final BoxConstraints constraints,
+  ) {
+    final Widget expando = IconButton(
+        onPressed: () {
+          onExpanded(!isExpanded);
+        },
+        icon: Icon(isExpanded ? Icons.expand_more : Icons.expand_less));
 
-    return Container(
-      decoration: BoxDecoration(
-        color: isSelected ? Theme.of(context).colorScheme.secondaryContainer : null,
-        borderRadius: const BorderRadius.horizontal(
-          left: Radius.circular(50),
-          right: Radius.circular(50),
-        ),
-      ),
-      child: TextButton(
-          onPressed: () {
-            // is use tap directly on one of the tabs
+    return Row(children: <Widget>[
+      expando,
+      const Spacer(),
+      // List of tab buttons
+      SegmentedButton<int>(
+        style: const ButtonStyle(visualDensity: VisualDensity(horizontal: -4, vertical: -4)),
+          segments: <ButtonSegment<int>>[
+            ButtonSegment<int>(
+                value: 0,
+                label: constraints.maxWidth < 600 ? null : const Text('Details'),
+                icon: const Icon(Icons.info_outline)),
+            ButtonSegment<int>(
+              value: 1,
+              label: constraints.maxWidth < 600 ? null : const Text('Chart'),
+              icon: const Icon(Icons.bar_chart),
+            ),
+            ButtonSegment<int>(
+              value: 2,
+              label: constraints.maxWidth < 600 ? null : const Text('Transactions'),
+              icon: const Icon(Icons.calendar_view_day),
+            ),
+          ],
+          selected: <int>{
+            selectedTabId
+          },
+          onSelectionChanged: (final Set<int> newSelection) {
             if (!isExpanded) {
               onExpanded(true);
             }
-            onTabActivated(id);
-          },
-          style: TextButton.styleFrom(
-            textStyle: TextStyle(fontWeight: isSelected ? FontWeight.bold : FontWeight.normal),
-          ),
-          child: Text(text)),
-    );
-  }
-
-  Widget getRowOfTabs(final BuildContext context) {
-    return Row(
-      children: <Widget>[
-        getTabButton(context, 0, 'Details'),
-        getTabButton(context, 1, 'Chart'),
-        getTabButton(context, 2, 'Transactions'),
-      ],
-    );
-  }
-
-  Widget _buildTabs(final BuildContext context) {
-    return Row(children: <Widget>[
-      // List of tab buttons
-      Expanded(child: getRowOfTabs(context)),
-
+            onTabActivated(newSelection.first);
+          }),
+      const Spacer(),
       // Expando
-      IconButton(
-          onPressed: () {
-            onExpanded(!isExpanded);
-          },
-          icon: Icon(isExpanded ? Icons.expand_more : Icons.expand_less))
+      expando,
     ]);
   }
 
