bf664b2e55a4cf1fab07c8d9f1c6bdc0d35399fb
JP <jp@vteam.com>
2024-03-18 13:56:28 +0000

    add script for generating Lakos dot graphs

---
+++ graph.sh
@@ -0,0 +1,18 @@
+# install lakos - see https://pub.dev/packages/lakos/install
+# dart pub global activate lakos
+# export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+rm graph.dot
+rm graph.svg
+
+## with folders
+lakos -o graph.dot --metrics --ignore=test/** .
+
+# remove the folders
+#lakos -o graph.dot --no-tree --metrics --ignore=test/** .
+
+
+dot -Tsvg graph.dot -Grankdir=TB -Gcolor=lightgray -Ecolor="#aabbaa88" -o graph.svg
+#fdp -Tsvg graph.dot -Gcolor=lightgray -Ecolor="#aabbaa99" -o graph.svg
+
+open graph.svg
