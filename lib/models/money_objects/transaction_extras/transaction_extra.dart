00db2cfef511c9c8ce80af5a34c07bd7385b67c6
jpdup <jp.duplessis@teya.com>
2024-03-23 20:30:46 +0000

    load TransactionExtra

--- lib/models/money_objects/transaction_extras/transaction_extra.dart
+++ lib/models/money_objects/transaction_extras/transaction_extra.dart
@@ -11,32 +11,48 @@ import 'package:money/models/money_objects/money_object.dart';
 class TransactionExtra extends MoneyObject {
   @override
   int get uniqueId => id.value;
+
   @override
   set uniqueId(value) => id.value = value;
 
-  // 0
+  /// ID
+  /// SQLite  0|Id|bigint|0||1
   FieldId<TransactionExtra> id = FieldId<TransactionExtra>(
     valueForSerialization: (final TransactionExtra instance) => instance.uniqueId,
   );
 
   // 1
-  int transaction;
+  FieldInt<TransactionExtra> transaction = FieldInt<TransactionExtra>(
+    serializeName: 'Transaction',
+    valueForSerialization: (final TransactionExtra instance) => instance.transaction.value,
+  );
 
   // 2
-  int taxYear;
+  FieldInt<TransactionExtra> taxYear = FieldInt<TransactionExtra>(
+    serializeName: 'TaxYear',
+    valueForSerialization: (final TransactionExtra instance) => instance.taxYear.value,
+  );
 
   // 4
-  int taxDate;
+  FieldDate<TransactionExtra> taxDate = FieldDate<TransactionExtra>(
+    serializeName: 'TaxDate',
+    valueForSerialization: (final TransactionExtra instance) => instance.taxDate.value,
+  );
 
   /// Constructor
   TransactionExtra({
     // 0
-    // id
+    required int id,
     // 1
-    required this.transaction,
+    required int transaction,
     // 2
-    required this.taxYear,
+    required int taxYear,
     // 3
-    required this.taxDate,
-  });
+    required DateTime? taxDate,
+  }) {
+    this.id.value = id;
+    this.transaction.value = transaction;
+    this.taxYear.value = taxYear;
+    this.taxDate.value = taxDate;
+  }
 }
