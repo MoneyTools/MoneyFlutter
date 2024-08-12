76d5d896e014d7da12a1575f2a433e9562ec0c2a
JP <2709213+jpdup@users.noreply.github.com>
2024-08-12 13:03:24 +0100

    tweak test run to retain the lcov.info

--- sh/test_integration.sh
+++ sh/test_integration.sh
@@ -2,7 +2,7 @@
 
 rm coverage/lcov1.info &> /dev/null
 rm coverage/lcov2.info &> /dev/null
-rm coverage/lcovCombined.info &> /dev/null
+rm coverage/lcov.info &> /dev/null
 
 flutter test integration_test --coverage -d macos
 mv coverage/lcov.info coverage/lcov1.info
@@ -10,9 +10,9 @@ mv coverage/lcov.info coverage/lcov1.info
 flutter test --coverage 
 mv coverage/lcov.info coverage/lcov2.info
 
-lcov -a coverage/lcov1.info -a coverage/lcov2.info -o coverage/lcovCombined.info
+lcov -a coverage/lcov1.info -a coverage/lcov2.info -o coverage/lcov.info
 
-genhtml -q coverage/lcovCombined.info -o coverage/html > coverage/cc.txt
+genhtml -q coverage/lcov.info -o coverage/html > coverage/cc.txt
 cat coverage/cc.txt
 
 open coverage/html/index.html
