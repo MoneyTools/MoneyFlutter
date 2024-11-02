2a4c8f5b2b6573c9d49c55fbc3f8c78f607fb232
JP <2709213+jpdup@users.noreply.github.com>
2024-11-02 13:33:30 -0700

    update cmake for linux and windows

--- linux/flutter/generated_plugins.cmake
+++ linux/flutter/generated_plugins.cmake
@@ -5,7 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
   pasteboard
-  screen_retriever
+  screen_retriever_linux
   sqlite3_flutter_libs
   url_launcher_linux
   window_manager
--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -5,7 +5,7 @@
 list(APPEND FLUTTER_PLUGIN_LIST
   desktop_drop
   pasteboard
-  screen_retriever
+  screen_retriever_windows
   sqlite3_flutter_libs
   url_launcher_windows
   window_manager
