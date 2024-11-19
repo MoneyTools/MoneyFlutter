51819cf417af99e10b94a39c105940b77fb9276d
JPDup <jp@vteam.com>
2024-11-18 16:38:16 -0800

    fix genhtml remove > --no-function-coverage

--- tool/test_integration.sh
+++ tool/test_integration.sh
@@ -6,8 +6,9 @@ flutter test --coverage --coverage-path=coverage/lcov_units.info || exit 1
 
 lcov -a coverage/lcov_integration.info -a coverage/lcov_units.info -o coverage/lcov.info
 
-genhtml --no-function-coverage --css-file coverage/genhtml.css  -q coverage/lcov.info -o coverage/html > coverage/cc.txt
+genhtml --css-file coverage/genhtml.css  -q coverage/lcov.info -o coverage/html > coverage/cc.txt
 
+# keep the file cc.txt in git log, but also display it to the user
 cat coverage/cc.txt
 
 open coverage/html/index.html
